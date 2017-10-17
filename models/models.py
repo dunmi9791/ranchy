0b517132daeffd1c2949a74cc399da32e50abbf7
dunmi9791 <dominic.anyanna@gmail.com>
2017-10-17 16:37:31 +0100

    modified:   models/models.py

--- models/models.py
+++ models/models.py
@@ -23,13 +23,16 @@ class ranchy_corps(models.Model):
 class ranchy_loans(models.Model):
     _name = 'ranchy.loans'
     loan_id = fields.Id
-    amount = fields.Monetary(string="Amount",# optional: currency_field='currency_id')
-    disburse_date = fields.Date(string="Date of Disburse")
-    member_id = fields.Many2one('members.ranchy', string="Member")
-    tenure = fields.Char(string="loan tenure")
-    
     total_repayments = fields.Float(string="Total repayed")
     balance = fields.Float(string="Outstanding Balance")
+    stage = fields.selection([('1st','2nd')])
+    amount = fields.Monetary(string="Amount",# optional: currency_field='currency_id')
+    disburse_date = fields.Date(string="Date of Disburse")
+    
+   
+    
+    
+    
     
     
        
