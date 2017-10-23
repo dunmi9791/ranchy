66d691b595584953ceff4332fbb3f167fdb89d3b
dunmi9791 <dominic.anyanna@gmail.com>
2017-10-23 16:32:23 +0100

    modified:   models/models.py 	modified:   views/views.xml

--- models/models.py
+++ models/models.py
@@ -37,7 +37,10 @@ class ranchy_corps(models.Model):
     corps_staff = fields.One2many
     
 class ranchy_loans(models.Model):
-    _name = 'ranchy.loans'
+    _name = 'ranchy.loans' 
+    
+    
+    
     loan_id = fields.Id
     currency_id = fields.Many2one(
         'res.currency', string='Currency')
@@ -49,10 +52,23 @@ class ranchy_loans(models.Model):
         'Amount',
         # optional: currency_field='currency_id',
         )
+    amount_applied = fields.Monetary(
+        'Amount applied for',
+        # optional: currency_field='currency_id',
+        )
+    amount_approved = fields.Monetary(
+        'Amount approved',
+        # optional: currency_field='currency_id',
+        )
     amount_balance = fields.Monetary(
         'Balance',
         # optional: currency_field='currency_id',
         )
+    last_loan = fields.Monetary(
+        'last loan recieved amount',
+        # optional: currency_field='currency_id',
+        )
+    date_payed = fields.Date('Date loan was fully paid')
     date_disburse = fields.Date('Date')
     member_id = fields.Many2one(
         'members.ranchy', string='Member',
@@ -64,6 +80,17 @@ class ranchy_loans(models.Model):
     stage = fields.Selection(
         [('1st','First'), ('2nd','Second'),('3rd','third')],
         'Stage')
+    installments = fields.Selection(
+        [('23','23'), ('20','20'),('18','18')],
+        'number of installments')
+    date_firstinstall = fields.Date('Date of first installment')
+    date_finalinstall = fields.Date('Date of last installment')
+    any_family = fields.Boolean('Is any family member of yours registered with the group')
+    if_true = fields.Char('If true mention name')
+    or_reg = fields.Boolean('Or registered in any other group of ranchi empowerment centre')
+    indebted = fields.Boolean('Are you indebted to any otherMFB/MFI')
+    if_true2 = fields.Char('If true Name')
+    if_true3 = fields.Char('Outstanding Balance')
        
 class ranchy_repayments(models.Model):
     _name = 'ranchy.repayments'
